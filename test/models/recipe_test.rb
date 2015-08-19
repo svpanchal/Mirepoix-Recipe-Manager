542ba6e9205e9beb521760ffc2e1ceaf97d60ca9
Kiffin Gish <kiffin.gish@planet.nl>
2015-08-19 21:30:25 +0200

    TDD for recipe model, spec and validation.

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  def setup
+    @recipe = Recipe.new(name: 'chicken parm', summary: 'this is the best chicken parm ever',
+                         description: 'heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes')
+  end
+
+  test 'recipe should be valid' do
+    assert @recipe.valid?
+  end
+
+  test 'name should be present' do
+    @recipe.name = ' '
+    assert_not @recipe.valid?
+  end
+
+  test 'name should not be too long' do
+    @recipe.name = 'a' * 101
+    assert_not @recipe.valid?
+  end
+
+  test 'name should not be too short' do
+    @recipe.name = 'aaaa'
+    assert_not @recipe.valid?
+  end
+
+  test 'summary must be present' do
+    @recipe.summary = ' '
+    assert_not @recipe.valid?
+  end
+
+  test 'summary should not be too long' do
+    @recipe.summary = 'a' * 151
+    assert_not @recipe.valid?
+  end
+
+  test 'summary should not be too short' do
+    @recipe.summary = 'a' * 9
+    assert_not @recipe.valid?
+  end
+
+  test 'description must be present' do
+    @recipe.description = ' '
+    assert_not @recipe.valid?
+  end
+
+  test 'description should not be too long' do
+    @recipe.description = 'a' * 501
+    assert_not @recipe.valid?
+  end
+
+  test 'description should not be too short' do
+    @recipe.description = 'a' * 19
+    assert_not @recipe.valid?
+  end
+
+end
