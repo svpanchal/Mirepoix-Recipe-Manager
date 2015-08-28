de711ba710347bb106362dec5f66f2c6adcfc8f9
Kiffin Gish <kiffin.gish@planet.nl>
2015-08-28 12:08:49 +0200

    New recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,27 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    # TODO: Now hardcoded.
+    @recipe.chef = Chef.find(1)
+
+    if @recipe.save
+      flash[:success] = 'Your recipe was created successfully'
+      redirect_to recipes_path
+    else
+     render :new
+    end
+  end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
 end
