a448f6aecfdd0477620828fae283e7962946ef3c
Kiffin Gish <kiffin.gish@planet.nl>
2015-08-30 13:06:09 +0200

    Add image uploader for recipe pictures.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -43,6 +43,6 @@ class RecipesController < ApplicationController
   private
 
     def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description)
+      params.require(:recipe).permit(:name, :summary, :description, :picture)
     end
 end
